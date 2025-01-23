5c41364e1f35e29ddf6d2e5637bf825e07b2b818
harishb2006 <imharishba@gmail.com>
2025-01-23 17:28:49 +0530

    login

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import  LoginPage  from './Routes.js';
+import LoginPage from './pages/LoginPage';
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+
+export default App;
