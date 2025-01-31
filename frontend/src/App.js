e2298e72366d61e9ff678b99a7614784e1c8d0ab
harishb2006 <imharishba@gmail.com>
2025-01-31 15:25:22 +0530

    milestone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import  LoginPage  from './Routes.js';
-import { LoginPage } from './Routes.js';
-import { SignupPage } from './Routes.js';
+import { LoginPage,SignupPage } from './Routes.js';
+
+import {Home} from './Routes.js';
 import "./App.css";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path='/' element={< Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
       </Routes>
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,8 @@
 import LoginPage from "./pages/LoginPage";
 import  SignupPage  from "./pages/SignupPage";
+import Home from "./pages/Home"
 export {
     LoginPage,
-    SignupPage
+    SignupPage,
+    Home
 }
